697987ba8f83676e4bc4e843bb069cab30879e0f
x20c <vbachinin2018@gmail.com>
2026-04-13 15:52:28 +0300

    fix: overcorrection in task 1 range, update readme

--- phase-0-numpy/task-01-list-comp/solution.py
+++ phase-0-numpy/task-01-list-comp/solution.py
@@ -1,20 +1,20 @@
 # Task 1
 def squares_of_evens(n):
-    return [x*x for x in range(0,n-1) if x % 2 == 0]
+    return [x*x for x in range(n) if x % 2 == 0]
 
 
 # Task 2
 def label_temperatures(temps):
     return ['freezing' if x <= 0 else
-            'cold' if 1 < x < 15 else
-            'warm' if 16 < x < 25 else
+            'cold' if x <= 15 else
+            'warm' if x <= 25 else
             'hot'
             for x in temps]
 
 
 # Task 3
 def filter_long_words(words, min_len):
-    return [word.upper() for word in words if len(word) >= min_len]
+    return [word.upper() for word in words if len(word) > min_len]
 
 
 # Task 4
@@ -24,6 +24,6 @@ def flatten_pairs(pairs):
 
 if __name__ == '__main__':
     print(squares_of_evens(10))
-    print(label_temperatures([-5, 10, 20, 30]))
-    print(filter_long_words(["hi", "hello", "a", "python"], 3))
+    print(label_temperatures([0, 1, 15, 16, 25, 26, 15.5]))
+    print(filter_long_words(["cat", "dog", "fish"], 3))
     print(flatten_pairs([(1, 2), (3, 4), (5, 6)]))
